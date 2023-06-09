2d3cdab4c0bda7d1715e64a1f5ca57c2dd0c9419
ArachDyrr <1021548@hr.nl>
2023-06-09 12:40:46 +0200

    WIP selector

--- streamlit/batch_size.py
+++ streamlit/batch_size.py
@@ -65,9 +65,9 @@ def page3():
 
         st.title("Selected AQL General I, Lot size 250-500 Batchsize 32")
 
-        st.button("Accept", on_click=nextPage)
+        st.button("Accept AQL protocol", on_click=nextPage)
         
-        st.button("Reject", on_click=restart)
+        st.button("Reject AQL protocol", on_click=restart)
 
 #--------------------------------------------------
 
@@ -98,6 +98,46 @@ def page5():
         st.button("Process images", on_click=nextPage)
 
 #--------------------------------------------------
+def page6():
+
+    with page.container():
+
+        st.title("AI is working!")
+
+        # loop through images, display them and accept or reject them
+        rejectcounter = 0
+        acceptcounter = 0
+        button1 = st.button("Accept1")
+        button2 = st.button("Reject1")
+        for image in st.session_state.batch:
+                
+                with page.container():
+    
+                    st.image(image)
+
+
+                    # store accept or reject in a variable
+                    if button1:
+                        acceptcounter += 1
+                        print(acceptcounter)
+                    elif button2:
+                        rejectcounter += 1
+                        print(rejectcounter)    
+                    
+        # move to the next page
+        # st.button("Accept results => accepted: {acceptcounter} Rejected: {rejectcounter}'", on_click=nextPage)
+        # st.button("Reject results", on_click=WIP)
+
+
+
+#--------------------------------------------------
+def page7():
+
+    with page.container():
+
+        st.title('save results and print documents')
+
+        st.button("Return to start", on_click=restart)
 
 def page99():
     with page.container():
@@ -149,6 +189,13 @@ elif st.session_state.page == 5:
 
     page5()
 
+elif st.session_state.page == 6:
+    
+    page6()
+
+elif st.session_state.page == 7:
+    
+    page7()
 
 elif st.session_state.page >= 99:
 
