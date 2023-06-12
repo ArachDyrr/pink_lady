b7fecba90397a9749b2b8dd54d3e68d7a34bd281
ArachDyrr <1021548@hr.nl>
2023-06-13 00:13:24 +0200

    fun function to rename to roman numerals

---
+++ factory/modules/myFunctions.py
@@ -0,0 +1,25 @@
+# to convert a number to a roman numeral
+def number_to_roman(number):
+    roman_mapping = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: 'X',
+        9: 'IX',
+        5: 'V',
+        4: 'IV',
+        1: 'I'
+    }
+
+    roman_numeral = ''
+    for value, symbol in roman_mapping.items():
+        while number >= value:
+            roman_numeral += symbol
+            number -= value
+
+    return roman_numeral
