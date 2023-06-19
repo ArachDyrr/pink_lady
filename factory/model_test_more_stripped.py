1426f0c6f77e04d66161be90ddcab640a720c6df
ArachDyrr <1021548@hr.nl>
2023-06-20 00:16:37 +0200

    changed to show data used in GradCam_misla.....

--- factory/model_test_more_stripped.py
+++ factory/model_test_more_stripped.py
@@ -24,16 +24,16 @@ unedited = test_model_more(model, dataset_path, device, 32)
 print('#------------------------------------------------------------------------')
 print("more_test_stripped")
 print('#------------------------------------------------------------------------')
-dataset_path_360 = "./build/cropper/cropped/more_test/test"
+dataset_path_360 = "./storage/images/stripped/more_test/test"
 result_more_stripped= test_model_more(model, dataset_path_360, device, 32)
 # print(result_more)
 # print(type(result_more['Confusion Matrix']))
 
-print('#------------------------------------------------------------------------')
-print("more_test")
-print('#------------------------------------------------------------------------')
-dataset_path_360 = "./storage/images/more_apples/test"
-result_more= test_model_more(model, dataset_path_360, device, 32)
-# # print(result_more)
-# # print(type(result_more['Confusion Matrix']))
+# print('#------------------------------------------------------------------------')
+# print("more_test")
+# print('#------------------------------------------------------------------------')
+# dataset_path_360 = "./storage/images/more_apples/test"
+# result_more= test_model_more(model, dataset_path_360, device, 32)
+# # # print(result_more)
+# # # print(type(result_more['Confusion Matrix']))
 
